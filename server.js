4d79d000c14123805fd534e77d00ff89613eb0e1
Adrian <adrian.auch11@gmail.com>
2021-08-26 15:46:13 -0600

    initial commit with notes

---
+++ server.js
@@ -0,0 +1,13 @@
+// require in node modules
+
+// section for port  boiler plate
+
+// Middleware
+
+// GET route to return notes.html
+
+// GET route return index.html
+
+// Get to return the api/notes file
+
+// POST
